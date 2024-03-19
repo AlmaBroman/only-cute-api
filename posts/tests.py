38ecd30c2b103df625932964af7e7186849206af
Alma Broman <alma.broman@gmail.com>
2024-03-19 10:05:34 +0000

    Add tests and test only authenticated users can create, delete, update, retrieve post

--- posts/tests.py
+++ posts/tests.py
@@ -1,1 +1,60 @@
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='alex', password='pass')
+
+    def test_can_list_posts(self):
+        alex = User.objects.get(username='alex')
+        Post.objects.create(owner=alex, title='a title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='alex', password='pass')
+        response = self.client.post('/posts/', {'title': 'a title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_not_logged_in_cant_create_post(self):
+        response = self.client.post('/posts/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        alex = User.objects.create_user(username='alex', password='pass')
+        molly = User.objects.create_user(username='molly', password='pass')
+        Post.objects.create(
+            owner=alex, title='alex title', content='alexs content'
+        )
+        Post.objects.create(
+            owner=molly, title='mollys title', content='mollys content'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'alex title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_user_can_update_own_post(self):
+        self.client.login(username='alex', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='molly', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
