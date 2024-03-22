5514fd1b030325b227d7cc6912bedb19eb27f9c8
Alma Broman <alma.broman@gmail.com>
2024-03-22 13:22:37 +0000

    edit saved post model

--- saved_posts/models.py
+++ saved_posts/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from posts.models import Post
 
 class SavedPost(models.Model):
-    owner = models.ForeignKey(User, related_name='saved_posts', on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='saved_posts', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
--- saved_posts/serializers.py
+++ saved_posts/serializers.py
@@ -1,7 +1,13 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import SavedPost
 
 class SavedPostSerializer(serializers.ModelSerializer):
+    """
+        Serialier for the Saved Post model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
     class Meta:
         model = SavedPost
         fields = ['id', 'created_at', 'owner', 'post']
