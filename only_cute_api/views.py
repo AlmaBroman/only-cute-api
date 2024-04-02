c86eddbf73373850f40d1f15965cf59ced00a3cc
Alma Broman <alma.broman@gmail.com>
2024-04-02 19:26:38 +0000

    Fix dj-rest auth bug that doesnt let users log out

--- only_cute_api/views.py
+++ only_cute_api/views.py
@@ -1,5 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE, 
+    JWT_AUTH_SECURE,
+)
 
 
 @api_view()
@@ -7,3 +11,17 @@ def root_route(request):
     return Response({
         'message':  'Welcome to Only Cute API!'
     })
+
+@apiview(['POST'])
+def logout:route(request):
+    response=Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
