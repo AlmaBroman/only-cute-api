7f8c7bfb64b03980c81fbbd66511844f61b316fe
Alma Broman <alma.broman@gmail.com>
2024-04-04 09:33:53 +0000

    Try configuring the postserializer image field

--- posts/serializers.py
+++ posts/serializers.py
@@ -13,6 +13,7 @@ class PostSerializer(serializers.ModelSerializer):
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
     saved_post_id = serializers.SerializerMethodField()
+    image =  image = serializers.ImageField(required=False, allow_null=True)
 
     def validate_image(self, value):
         """
