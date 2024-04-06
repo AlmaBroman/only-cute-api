07099cdd54a3a9bef868abfbf655c9f43490f4a7
Alma Broman <alma.broman@gmail.com>
2024-04-06 10:19:11 +0000

    test post has number of likes, comments and the string representation

--- posts/tests.py
+++ posts/tests.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import User
+from django.test import TestCase
 from .models import Post
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from posts.models import Post
+from likes.models import Like
+from comments.models import Comment
 
 
 class PostListViewTests(APITestCase):
@@ -59,3 +64,57 @@ class PostDetailViewTests(APITestCase):
         self.client.login(username='molly', password='pass')
         response = self.client.put('/posts/1/', {'title': 'a new title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostLikesTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test_user', password='password123'
+            )
+        self.client.force_authenticate(user=self.user)
+        self.post = Post.objects.create(
+            owner=self.user, title='Test Post', content='Test Content'
+            )
+
+    def test_post_has_number_of_likes(self):
+        existing_like = Like.objects.filter(
+            owner=self.user, post=self.post
+            ).exists()
+
+        if not existing_like:
+            Like.objects.create(owner=self.user, post=self.post)
+
+        response = self.client.get(f'/posts/{self.post.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['likes_count'], 1)
+
+
+class PostCommentsTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test_user', password='password123'
+            )
+        self.client.force_authenticate(user=self.user)
+        self.post = Post.objects.create(
+            owner=self.user, title='Test Post', content='Test Content'
+            )
+
+    def test_post_has_number_of_comments(self):
+        for i in range(5):
+            Comment.objects.create(
+                owner=self.user, post=self.post, content=f'Test Comment {i}'
+                )
+
+        response = self.client.get(f'/posts/{self.post.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['comments_count'], 5)
+
+
+class PostModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user', password='password123')
+        self.post = Post.objects.create(owner=self.user, title='Test Post', content='Test Content')
+
+    def test_article_string_representation(self):
+        expected_representation = f'{self.post.id} {self.post.title}'
+        self.assertEqual(str(self.post), expected_representation)
